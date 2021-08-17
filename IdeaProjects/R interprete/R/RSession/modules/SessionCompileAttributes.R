f73eb419db69649d9db5f8c66295d532c0ccb1f4
mzhfei <MZHFEI@OUTLOOK.COM>
2021-08-17 14:59:50 -0600

    Initialize

--- IdeaProjects/R interprete/R/RSession/modules/SessionCompileAttributes.R
+++
@@ -1,8 +0,0 @@
-
-updated <- Rcpp::compileAttributes()
-wd <- normalizePath(".", winslash = "/")
-for (file in updated) {
-  file <- substr(file, nchar(wd)+2, nchar(file))
-  cat("* Updated ", file, "\n", sep="")
-}
-
--- IdeaProjects/test/build.sbt
+++
@@ -1,5 +0,0 @@
-name := "test"
-
-version := "0.1"
-
-scalaVersion := "2.12.7"
